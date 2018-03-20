23e695a89754d54873d3d88a038af2787966b6ef
Benson Chen <zjxucb@gmail.com>
2018-03-20 18:10:57 +0800

    2018-3-20 add XAspect 代码注入功能

---
+++ MobileFrame/Resource/AppDelegate/XAspect-LogAppDelegate.m
@@ -0,0 +1,51 @@
+//
+//  XAspect-LogAppDelegate.m
+//  MobileFrame
+//  XAspect Log AppDelegate 代码注入
+//  Created by Benson on 2018/3/20.
+//  Copyright © 2018年 Benson. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "XAspect.h"
+
+/**
+ 必须要有的宏定义, 而且必须写在最前面
+ @param AtAspect: 字段固定, 不能修改
+ @param LZProjectName: 这个可以自定义, 一般和自己需要注入代码的类有关
+*/
+#define AtAspect LogAppDelegate
+
+/** 
+ 需要注入代码的类
+ @param AtAspectOfClass: 固定字段, 不能修改
+ @param AppDelegate: 需要注入代码的类
+ */
+#define AtAspectOfClass AppDelegate
+// 开辟空间, 这个参数类名需和上面的类一致
+@classPatchField(AppDelegate)
+
+/**
+ 需要添加的方法
+ @param - 方法类型: - 实例方法; + 类方法
+ @param BOOL 返回值类型
+ @param UIApplication 方法实体
+ @return 返回
+ */
+AspectPatch(-, BOOL, application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions)
+{
+    //如果是测试本地版本开启调试工具FLEX
+    
+    //日志初始化
+    NSLog(@"日志初始化");
+    return XAMessageForward(application:application didFinishLaunchingWithOptions:launchOptions);
+}
+
+AspectPatch(-,void,applicationWillResignActive:(UIApplication *)application)
+{
+    NSLog(@"applicationWillResignActive");
+}
+
+@end
+#undef AtAspectOfClass
+#undef AtAspect
---
+++ MobileFrame/Resource/Global/XAspect-UIViewController.m
@@ -0,0 +1,37 @@
+//
+//  XAspect-UIViewController.m
+//  MobileFrame
+//
+//  Created by Benson on 2018/3/20.
+//  Copyright © 2018年 Benson. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "XAspect.h"
+
+#define AtAspect UIViewController
+
+#define AtAspectOfClass UIViewController
+@classPatchField(UIViewController)
+
+@synthesizeNucleusPatch(Default, -, void, viewDidLoad);
+@synthesizeNucleusPatch(Default, -, void, viewDidAppear:(BOOL)animated);
+
+AspectPatch(-, void, viewDidLoad){
+    
+    NSLog(@"[Benson]: %@'s view did load.", NSStringFromClass([self class]));
+    
+    return XAMessageForward(viewDidLoad);
+}
+
+//AspectPatch(-, void, viewDidAppear:(BOOL)animated){
+//    
+//    NSLog(@"[CocoaLumberjack Log]: %@'s view did appear.", NSStringFromClass([self class]));
+//    
+//    return XAMessageForward(viewDidAppear:(BOOL)animated);
+//}
+
+@end
+
+#undef AtAspectOfClass
+#undef AtAspect
