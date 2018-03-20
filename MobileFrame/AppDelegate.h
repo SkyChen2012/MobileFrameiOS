acfba4339597bca0ec292453bbcf8adc739a913b
Poppy <zjxucb@gmail.com>
2018-03-20 15:34:54 +0800

    2018-3-20

---
+++ MobileFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileFrame
+//
+//  Created by Honyar Intelligence on 2018/3/20.
+//  Copyright © 2018年 Benson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
